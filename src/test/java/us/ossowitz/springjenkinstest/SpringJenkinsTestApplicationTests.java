d66579cd844efb3f3f4d6135e0f167e9e9dae0ef
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-09-06 21:29:34 +0300

    Add JenkinsTest module

---
+++ src/test/java/us/ossowitz/springjenkinstest/SpringJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package us.ossowitz.springjenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
