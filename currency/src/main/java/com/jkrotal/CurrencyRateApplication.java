a88d25bc2f5c3b8b13953a219192aac57ae32982
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-20 04:08:30 +0300

    Change approach in configuration properties

--- currency/src/main/java/com/jkrotal/CurrencyRateApplication.java
+++ currency/src/main/java/com/jkrotal/CurrencyRateApplication.java
@@ -1,9 +1,12 @@
 package com.jkrotal;
 
+import com.jkrotal.currency.config.CurrencyClientCfg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CurrencyClientCfg.class)
 public class CurrencyRateApplication {
 
     public static void main(String[] args) {
