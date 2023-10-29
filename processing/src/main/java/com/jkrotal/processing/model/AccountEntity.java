4e1329543f1f3c0ade333cf8bae5e13c2fee3655
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-30 01:34:52 +0300

    Change fields AccountEntity

--- processing/src/main/java/com/jkrotal/processing/model/AccountEntity.java
+++ processing/src/main/java/com/jkrotal/processing/model/AccountEntity.java
@@ -1,9 +1,6 @@
 package com.jkrotal.processing.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +12,8 @@ import static jakarta.persistence.GenerationType.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "Account")
+@Entity
+@Table(name = "account")
 public class AccountEntity {
 
     @Id
@@ -29,6 +27,6 @@ public class AccountEntity {
     @Column(name = "currency_code", length = 3, nullable = false)
     private String currencyCode;
 
-    @Column(name = "balance", length = 3, nullable = false)
+    @Column(name = "balance", nullable = false)
     private BigDecimal balance;
 }
