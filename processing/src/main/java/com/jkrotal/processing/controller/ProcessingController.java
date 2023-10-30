ce37533546b738fac2f6086fe9033f1b7a5b8e16
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-30 08:37:37 +0300

    Add createAccount() method in ProcessingController

--- processing/src/main/java/com/jkrotal/processing/controller/ProcessingController.java
+++ processing/src/main/java/com/jkrotal/processing/controller/ProcessingController.java
@@ -1,6 +1,11 @@
 package com.jkrotal.processing.controller;
 
+import com.jkrotal.processing.dto.NewAccountDTO;
+import com.jkrotal.processing.model.AccountEntity;
+import com.jkrotal.processing.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/processing")
 public class ProcessingController {
 
+    private final AccountService accountService;
+
+    @PostMapping("/account")
+    public AccountEntity createAccount(@RequestBody NewAccountDTO account) {
+        return accountService.createNewAccount(account);
+    }
 }
