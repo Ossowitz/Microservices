305c2d02343713ce5d18183c9ba0378876d66f7e
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-30 04:07:39 +0300

    Create base for ProcessingController

---
+++ processing/src/main/java/com/jkrotal/processing/controller/ProcessingController.java
@@ -0,0 +1,12 @@
+package com.jkrotal.processing.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/processing")
+public class ProcessingController {
+
+}
