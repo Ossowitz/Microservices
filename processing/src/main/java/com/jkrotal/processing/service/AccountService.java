5828b339cf599187149b76bb147898b7d863348a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-31 07:59:04 +0300

    Add addMoneyToAccount() method for AccountEntity

--- processing/src/main/java/com/jkrotal/processing/service/AccountService.java
+++ processing/src/main/java/com/jkrotal/processing/service/AccountService.java
@@ -1,13 +1,18 @@
 package com.jkrotal.processing.service;
 
 import com.jkrotal.processing.dto.NewAccountDTO;
+import com.jkrotal.processing.exception.AccountNotFoundException;
 import com.jkrotal.processing.model.AccountEntity;
+import com.jkrotal.processing.model.Operation;
 import com.jkrotal.processing.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +29,17 @@ public class AccountService {
                 .build();
 
         return accountRepository.save(account);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    public AccountEntity addMoneyToAccount(String uid, Long accountId, Long targetAccount,
+                                           Operation operation, BigDecimal money) {
+        Optional<AccountEntity> account = accountRepository.findById(accountId);
+        return account.map(it -> {
+            BigDecimal balance = it.getBalance().add(money);
+            it.setBalance(balance);
+
+            return accountRepository.save(it);
+        }).orElseThrow(() -> new AccountNotFoundException(accountId));
     }
 }
