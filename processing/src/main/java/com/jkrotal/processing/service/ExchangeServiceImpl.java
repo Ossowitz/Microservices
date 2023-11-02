f9538b8837dffa34bedf44d2c02cd6fb5e050f04
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-02 11:22:16 +0300

    Add ExchangeServiceImpl for exchange manipulation

--- processing/src/main/java/com/jkrotal/processing/service/ExchangeServiceImpl.java
+++ processing/src/main/java/com/jkrotal/processing/service/ExchangeServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static com.jkrotal.processing.model.Operation.*;
 
@@ -34,19 +35,35 @@ public class ExchangeServiceImpl implements ExchangeService {
 
         BigDecimal result;
 
-        /**
-         * If the sender's currency is not ruble (CURRENCY_RUB), and the recipient's currency is ruble,
-         *         we load the conversion rate and perform the exchange taking into account the coefficient
-         */
         if (!CURRENCY_RUB.equals(sourceAccount.getCurrencyCode())
             && CURRENCY_RUB.equals(targetAccount.getCurrencyCode())) {
             BigDecimal rate = currencyService.loadCurrencyRate(sourceAccount.getCurrencyCode());
             result = exchangeWithMultiply(uuid, sourceAccount, targetAccount, rate, amount);
+        } else if (CURRENCY_RUB.equals(sourceAccount.getCurrencyCode())
+                   && !CURRENCY_RUB.equals(targetAccount.getCurrencyCode())) {
+            BigDecimal rate = currencyService.loadCurrencyRate(targetAccount.getCurrencyCode());
+            BigDecimal multiplier = new BigDecimal(1).divide(rate, 4, RoundingMode.HALF_DOWN);
+            result = exchangeWithMultiply(uuid, sourceAccount, targetAccount, multiplier, amount);
+        } else if (!CURRENCY_RUB.equals(sourceAccount.getCurrencyCode()) && !CURRENCY_RUB.equals(targetAccount.getCurrencyCode())) {
+            BigDecimal rateFrom = currencyService.loadCurrencyRate(sourceAccount.getCurrencyCode());
+            BigDecimal rateTo = currencyService.loadCurrencyRate(targetAccount.getCurrencyCode());
+            result = exchangeThroughRub(uuid, sourceAccount, targetAccount, rateFrom, rateTo, amount);
+        } else if (CURRENCY_RUB.equals(sourceAccount.getCurrencyCode()) && CURRENCY_RUB.equals(targetAccount.getCurrencyCode())) {
+            result = simpleExchange(uuid, sourceAccount, targetAccount, amount);
+        } else {
+            throw new IllegalStateException("Unknown behavior");
         }
 
         LOGGER.info("Exchange operation {} from account {} to account {} completed", uuid, fromAccount, toAccount);
 
-        return null;
+        return result;
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public BigDecimal simpleExchange(String uuid, AccountEntity sourceAccount, AccountEntity targetAccount, BigDecimal amount) {
+        accountService.addMoneyToAccount(uuid, sourceAccount.getId(), targetAccount.getId(), EXCHANGE, amount.negate());
+        accountService.addMoneyToAccount(uuid, targetAccount.getId(), sourceAccount.getId(), EXCHANGE, amount);
+        return amount;
     }
 
     @Transactional(isolation = Isolation.REPEATABLE_READ)
@@ -54,6 +71,16 @@ public class ExchangeServiceImpl implements ExchangeService {
                                            BigDecimal rate, BigDecimal amount) {
         accountService.addMoneyToAccount(uuid, sourceAccount.getId(), targetAccount.getId(), EXCHANGE, amount.negate());
         BigDecimal result = amount.multiply(rate);
+        accountService.addMoneyToAccount(uuid, targetAccount.getId(), sourceAccount.getId(), EXCHANGE, result);
+        return result;
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public BigDecimal exchangeThroughRub(String uuid, AccountEntity sourceAccount, AccountEntity targetAccount,
+                                         BigDecimal rateFrom, BigDecimal rateTo, BigDecimal amount) {
+        accountService.addMoneyToAccount(uuid, sourceAccount.getId(), targetAccount.getId(), EXCHANGE, amount.negate());
+        BigDecimal rub = amount.multiply(rateFrom);
+        BigDecimal result = rub.divide(rateTo, 4, RoundingMode.HALF_DOWN);
         accountService.addMoneyToAccount(uuid, targetAccount.getId(), sourceAccount.getId(), EXCHANGE, result);
         return result;
     }
