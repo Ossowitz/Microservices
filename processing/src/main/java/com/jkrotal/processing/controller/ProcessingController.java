229c90a3af96ad3b443aa9f057b332f84bd5f6bc
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-02 15:59:18 +0300

    Add exchange method for money

--- processing/src/main/java/com/jkrotal/processing/controller/ProcessingController.java
+++ processing/src/main/java/com/jkrotal/processing/controller/ProcessingController.java
@@ -1,13 +1,16 @@
 package com.jkrotal.processing.controller;
 
+import com.jkrotal.processing.dto.ExchangeMoneyDTO;
 import com.jkrotal.processing.dto.NewAccountDTO;
 import com.jkrotal.processing.dto.PutAccountDTO;
 import com.jkrotal.processing.model.AccountEntity;
 import com.jkrotal.processing.model.Operation;
 import com.jkrotal.processing.service.AccountService;
+import com.jkrotal.processing.service.ExchangeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class ProcessingController {
 
     private final AccountService accountService;
+
+    private final ExchangeService exchangeService;
 
     @PostMapping("/account")
     public AccountEntity createAccount(@RequestBody NewAccountDTO account) {
@@ -26,4 +31,8 @@ public class ProcessingController {
         return accountService.addMoneyToAccount(data.getUuid(), accountId, null, Operation.PUT, data.getMoney());
     }
 
+    @PutMapping("/exchange/{uid}")
+    public BigDecimal exchangeMoney(@PathVariable("uid") String uid, @RequestBody ExchangeMoneyDTO data) {
+        return exchangeService.exchangeCurrency(uid, data.getFromAccountId(), data.getToAccountId(), data.getMoney());
+    }
 }
