8ce30ce5538314639043c01e106d81c19ce38296
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-03 00:20:54 +0300

    Add currency microservice how Eureka Client

--- currency/src/main/java/com/jkrotal/currency/CurrencyRateApplication.java
+++ currency/src/main/java/com/jkrotal/currency/CurrencyRateApplication.java
@@ -4,8 +4,10 @@ import com.jkrotal.currency.config.CurrencyClientCfg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigurationProperties(CurrencyClientCfg.class)
 public class CurrencyRateApplication {
 
